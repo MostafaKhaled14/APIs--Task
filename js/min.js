08f112b69ef645671e8b4fed631501078e86334d
mostafa <mostafa2061999z13@gmail.com>
2024-12-15 00:27:02 +0200

    footer section commit

--- js/min.js
+++ js/min.js
@@ -1,7 +1,19 @@
 
 
 let links = document.querySelectorAll('.collapse .nav-item a');
+let list =document.querySelector('#navbarSupportedContent');
+let listOfLinks =document.querySelectorAll('#navbarSupportedContent a');
 
+
+function removListOfLinks(){
+    list.classList.remove('show');
+}
+
+document.addEventListener('click', removListOfLinks)
+
+for (let k = 0; k < listOfLinks.length; k++) {
+    listOfLinks[k].addEventListener('click', removListOfLinks);
+}
 
 let changeActive = (j) =>{
     for (let i = 0; i < links.length; i++) {
